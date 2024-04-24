415161b862afce16d45a10971f0573b8da1eede2
CaplatMiruna <mctxy05@gmail.com>
2024-04-24 08:17:25 +0300

    Spring Init

---
+++ src/main/java/com/miru/WebFitnessApplication/WebFitnessApplication.java
@@ -0,0 +1,13 @@
+package com.miru.WebFitnessApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFitnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebFitnessApplication.class, args);
+	}
+
+}
